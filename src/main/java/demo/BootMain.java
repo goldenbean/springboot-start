ffa3a5ee9c44822d3a184a5a1acf731a7d6ba4c9
Jing Gao <jing.gao@outlook.com>
2021-05-10 15:11:39 +0800

    init repo

---
+++ src/main/java/demo/BootMain.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMain {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BootMain.class, args);
+  }
+
+}
