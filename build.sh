3347da2771bab5c6ec0f20306fa148aa1f40e460
Jing Gao <jing.gao@outlook.com>
2021-05-12 10:27:18 +0800

    update livy pom

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-mvn clean install
-scp server/target/springboot-start-0.0.1-SNAPSHOT.jar admin@hadoop-3:
+mvn clean install -DskipTests
+scp server/target/springboot-start-server-0.0.1-SNAPSHOT.jar admin@hadoop-3:
